67ae522409c37491cc7d4c7e014371017e45738b
Galina Marakina <g.marakina.2gis@gmail.com>
2020-07-05 16:17:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/callsend/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.callsend;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText phone;
+    private EditText message;
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 11;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 13;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        phone = findViewById(R.id.et_phone);
+        message = findViewById(R.id.et_message);
+    }
+
+    public void goToCall(View view) {
+        final String number = phone.getText().toString();
+        if (number.length() > 0) {
+            callByNumber();
+        } else {
+            Toast.makeText(MainActivity.this, R.string.enter_phone_number, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void callByNumber() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        } else {
+            String number = phone.getText().toString();
+            Intent openPhone = new Intent(Intent.ACTION_CALL);
+            Uri uri = Uri.parse("tel:" + number);
+            openPhone.setData(uri);
+            startActivity(openPhone);
+            startActivity(openPhone);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callByNumber();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    public void goToSend(View view) {
+//        Intent openSmsManager = new Intent(Intent.ACTION_VIEW);
+//        Uri uri;
+//        if (text.length() > 0 && number.length() > 0) {
+//            uri = Uri.parse("smsto:" + text);
+//            openSmsManager.setData(uri);
+//            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+//                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+//            } else {
+//                SmsManager smgr = SmsManager.getDefault();
+//                smgr.sendTextMessage(number,null,text,null,null);
+//                startActivity(openSmsManager);
+//            }
+//        } else {
+//            Toast.makeText(MainActivity.this, R.string.enter_text, Toast.LENGTH_SHORT).show();
+//        }
+    }
+}
